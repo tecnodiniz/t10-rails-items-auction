144689287da97fd2ded4a69c27c38a186e1869c4
Eduardo Diniz <diniz480@gmail.com>
2023-05-07 11:28:19 -0300

    Validação de itens

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :category
   before_create :generate_code
+  validates :name, :weight, :height, :width, :depth, :category_id, presence: true
+  validates :code, uniqueness: true
 
   private
   def generate_code 
