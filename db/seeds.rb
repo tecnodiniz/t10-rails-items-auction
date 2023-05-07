d1f0558b2570d1853adc9cbad5c6bbd228a39f75
Eduardo Diniz <diniz480@gmail.com>
2023-05-07 11:25:05 -0300

    Regra de négocio para Items: somente usuários administradores podem cadastrar novo item para leilão

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 User.create!(email:'diniz480@gmail.com', password: '@@l0ck3d0u7@@',cpf:'44047449865',admin:true)
+c = Category.create!(description: 'Eletro Doméstico')
+Category.create!(description: 'Roupas')
+
+Item.create!(name:'Televisão Samsung', url_img:'',weight:8000,height:70,width:90, 
+                depth:20,code:'ABXPA0-EJN22-IEDCVHX', category:c)
---
+++ spec/system/items/user_view_link_to_new_item_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe 'Usuário visita página de itens' do 
+    it 'e vê link para criar novos itens caso seja administrador' do 
+        user = User.create!(email:'eduardo@leilaodogalpao.com.br', password: 'password', cpf:'44047449865', admin: true)
+        login_as(user)
+
+
+        visit root_path
+
+        click_on 'Itens'
+
+        expect(page).to have_content 'Cadastrar item para leilão'
+
+    end
+    it 'e não vê link para criar novos itens caso não seja administrador' do 
+        user = User.create!(email:'eduardo@leilaodogalpao.com.br', password: 'password', cpf:'44047449865', admin: false)
+        login_as(user)
+
+
+        visit root_path
+
+        click_on 'Itens'
+
+        expect(page).not_to have_content 'Cadastrar item para leilão'
+
+    end
+
+    it 'e não vê link para criar novos itens caso não seja administrador e nem um usuário autenticado' do 
+        visit root_path
+
+        click_on 'Itens'
+
+        expect(page).not_to have_content 'Cadastrar item para leilão'
+
+    end
+
+end
