f815983a5036f6437c5fc8ee1c4b0c716b436c4a
Eduardo Diniz <diniz480@gmail.com>
2023-07-15 13:07:50 -0300

    Lógica do lance feita

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -3,4 +3,26 @@ class Bid < ApplicationRecord
   belongs_to :user
 
   validates :value, presence: true
+  validate :check_value
+  validate :check_dif_value
+
+  def check_value
+    return unless lot.min_value > value
+
+    errors.add(:base, "O valor mínimo para esse lote é R$#{lot.min_value}")
+    throw(:abort)
+  end
+
+  def check_dif_value
+    last_bid = Bid.where(lot:).last
+
+    return if last_bid.blank?
+
+    result = value - last_bid.value
+
+    return unless result < last_bid.lot.dif_value
+
+    errors.add(:base, "A diferença de valor para esse lance é de  R$#{lot.dif_value}")
+    throw(:abort)
+  end
 end
--- spec/system/bids/user_make_bid_spec.rb
+++ spec/system/bids/user_make_bid_spec.rb
@@ -1,40 +1,163 @@
 require 'rails_helper'
 
 feature 'Usuário faz um lance' do
-  scenario 'com sucesso' do
-    user = User.create!(email: 'user@gmail.com', cpf: '66978657689', password: 'password')
-    admin = Administrator.create!(email: 'admin@leilaodogalpao.com.br', cpf: '44047449865', password: 'password')
-    lot = Lot.create!(code: 'BAH295403', start_date: Time.zone.today, limit_date: 5.days.from_now,
-                      min_value: '400', dif_value: '400', administrator: admin, status: :aproved)
-    lot2 = Lot.create!(code: 'ZAR295403', start_date: 2.days.from_now, limit_date: 5.days.from_now,
-                       min_value: '400', dif_value: '400', administrator: admin, status: :aproved)
-    Lot.create!(code: 'HAZ295403', start_date: 2.days.from_now, limit_date: 5.days.from_now,
-                min_value: '400', dif_value: '400', administrator: admin, status: :awaiting)
+  context 'com sucesso' do
+    scenario 'primeiro lance ao lote' do
+      user = User.create!(email: 'user@gmail.com', cpf: '66978657689', password: 'password')
+      admin = Administrator.create!(email: 'admin@leilaodogalpao.com.br', cpf: '44047449865', password: 'password')
+      lot = Lot.create!(code: 'BAH295403', start_date: Time.zone.today, limit_date: 5.days.from_now,
+                        min_value: '400', dif_value: '400', administrator: admin, status: :aproved)
+      lot2 = Lot.create!(code: 'ZAR295403', start_date: 2.days.from_now, limit_date: 5.days.from_now,
+                         min_value: '400', dif_value: '400', administrator: admin, status: :aproved)
+      Lot.create!(code: 'HAZ295403', start_date: 2.days.from_now, limit_date: 5.days.from_now,
+                  min_value: '400', dif_value: '400', administrator: admin, status: :awaiting)
 
-    category = ProdCategory.create!(description: 'Doméstico')
-    category2 = ProdCategory.create!(description: 'Eletrônico')
-    logo = fixture_file_upload('spec/support/images/logo.png', 'image/png')
-    product = Product.create!(name: 'TV 32 Samsung', width: '30', height: '20', weight: '15', depth: '10',
-                              prod_category: category, logo:, status: :selected)
-    product2 = Product.create!(name: 'SOUNDBAR SONY', width: '30', height: '20', weight: '15', depth: '10',
-                               prod_category: category2, logo:, status: :selected)
+      category = ProdCategory.create!(description: 'Doméstico')
+      category2 = ProdCategory.create!(description: 'Eletrônico')
+      logo = fixture_file_upload('spec/support/images/logo.png', 'image/png')
+      product = Product.create!(name: 'TV 32 Samsung', width: '30', height: '20', weight: '15', depth: '10',
+                                prod_category: category, logo:, status: :selected)
+      product2 = Product.create!(name: 'SOUNDBAR SONY', width: '30', height: '20', weight: '15', depth: '10',
+                                 prod_category: category2, logo:, status: :selected)
 
-    LotItem.create!(lot:, product:)
-    LotItem.create!(lot: lot2, product: product2)
+      LotItem.create!(lot:, product:)
+      LotItem.create!(lot: lot2, product: product2)
 
-    login_as user, scope: :user
+      login_as user, scope: :user
 
-    visit root_path
+      visit root_path
 
-    click_on lot.code
+      click_on lot.code
 
-    click_on 'Dar lance'
+      click_on 'Dar lance'
 
-    fill_in 'Valor',	with: '400'
-    click_on 'Criar Lance'
+      fill_in 'Valor',	with: '400'
+      click_on 'Criar Lance'
 
-    expect(page).to have_content 'Lance realizado com sucesso'
-    expect(page).to have_content 'Seu lance: 400'
-    expect(page).to have_content 'Último lance: 400'
+      expect(page).to have_content 'Lance realizado com sucesso'
+      expect(page).to have_content 'Seu lance: 400'
+      expect(page).to have_content 'Último lance: 400'
+    end
+
+    scenario 'em um lote com um lance existente' do
+      user = User.create!(email: 'user@gmail.com', cpf: '66978657689', password: 'password')
+      user2 = User.create!(email: 'user2@gmail.com', cpf: '51027834833', password: 'password')
+
+      admin = Administrator.create!(email: 'admin@leilaodogalpao.com.br', cpf: '44047449865', password: 'password')
+      lot = Lot.create!(code: 'BAH295403', start_date: Time.zone.today, limit_date: 5.days.from_now,
+                        min_value: '400', dif_value: '400', administrator: admin, status: :aproved)
+      lot2 = Lot.create!(code: 'ZAR295403', start_date: 2.days.from_now, limit_date: 5.days.from_now,
+                         min_value: '400', dif_value: '400', administrator: admin, status: :aproved)
+      Lot.create!(code: 'HAZ295403', start_date: 2.days.from_now, limit_date: 5.days.from_now,
+                  min_value: '400', dif_value: '400', administrator: admin, status: :awaiting)
+
+      category = ProdCategory.create!(description: 'Doméstico')
+      category2 = ProdCategory.create!(description: 'Eletrônico')
+      logo = fixture_file_upload('spec/support/images/logo.png', 'image/png')
+      product = Product.create!(name: 'TV 32 Samsung', width: '30', height: '20', weight: '15', depth: '10',
+                                prod_category: category, logo:, status: :selected)
+      product2 = Product.create!(name: 'SOUNDBAR SONY', width: '30', height: '20', weight: '15', depth: '10',
+                                 prod_category: category2, logo:, status: :selected)
+
+      LotItem.create!(lot:, product:)
+      LotItem.create!(lot: lot2, product: product2)
+
+      Bid.create!(lot:, user: user2, value: 400)
+      Bid.create!(lot:, user: user2, value: 800)
+
+      login_as user, scope: :user
+
+      visit root_path
+
+      click_on lot.code
+
+      click_on 'Dar lance'
+
+      fill_in 'Valor',	with: '1200'
+      click_on 'Criar Lance'
+
+      expect(page).to have_content 'Lance realizado com sucesso'
+      expect(page).to have_content 'Seu lance: 1200'
+      expect(page).to have_content 'Último lance: 1200'
+    end
+  end
+
+  context 'e falha' do
+    scenario 'com lance menor que o valor mínimo' do
+      user = User.create!(email: 'user@gmail.com', cpf: '66978657689', password: 'password')
+      admin = Administrator.create!(email: 'admin@leilaodogalpao.com.br', cpf: '44047449865', password: 'password')
+      lot = Lot.create!(code: 'BAH295403', start_date: Time.zone.today, limit_date: 5.days.from_now,
+                        min_value: '400', dif_value: '400', administrator: admin, status: :aproved)
+      lot2 = Lot.create!(code: 'ZAR295403', start_date: 2.days.from_now, limit_date: 5.days.from_now,
+                         min_value: '400', dif_value: '400', administrator: admin, status: :aproved)
+      Lot.create!(code: 'HAZ295403', start_date: 2.days.from_now, limit_date: 5.days.from_now,
+                  min_value: '400', dif_value: '400', administrator: admin, status: :awaiting)
+
+      category = ProdCategory.create!(description: 'Doméstico')
+      category2 = ProdCategory.create!(description: 'Eletrônico')
+      logo = fixture_file_upload('spec/support/images/logo.png', 'image/png')
+      product = Product.create!(name: 'TV 32 Samsung', width: '30', height: '20', weight: '15', depth: '10',
+                                prod_category: category, logo:, status: :selected)
+      product2 = Product.create!(name: 'SOUNDBAR SONY', width: '30', height: '20', weight: '15', depth: '10',
+                                 prod_category: category2, logo:, status: :selected)
+
+      LotItem.create!(lot:, product:)
+      LotItem.create!(lot: lot2, product: product2)
+
+      login_as user, scope: :user
+
+      visit root_path
+
+      click_on lot.code
+
+      click_on 'Dar lance'
+
+      fill_in 'Valor',	with: '300'
+      click_on 'Criar Lance'
+
+      expect(page).to have_content 'Não foi possível fazer o lance'
+      expect(page).to have_content 'O valor mínimo para esse lote é R$400'
+    end
+
+    scenario 'com lance que a diferença mínima' do
+      user = User.create!(email: 'user@gmail.com', cpf: '66978657689', password: 'password')
+      user2 = User.create!(email: 'user2@gmail.com', cpf: '51027834833', password: 'password')
+
+      admin = Administrator.create!(email: 'admin@leilaodogalpao.com.br', cpf: '44047449865', password: 'password')
+      lot = Lot.create!(code: 'BAH295403', start_date: Time.zone.today, limit_date: 5.days.from_now,
+                        min_value: '400', dif_value: '400', administrator: admin, status: :aproved)
+      lot2 = Lot.create!(code: 'ZAR295403', start_date: 2.days.from_now, limit_date: 5.days.from_now,
+                         min_value: '400', dif_value: '400', administrator: admin, status: :aproved)
+      Lot.create!(code: 'HAZ295403', start_date: 2.days.from_now, limit_date: 5.days.from_now,
+                  min_value: '400', dif_value: '400', administrator: admin, status: :awaiting)
+
+      category = ProdCategory.create!(description: 'Doméstico')
+      category2 = ProdCategory.create!(description: 'Eletrônico')
+      logo = fixture_file_upload('spec/support/images/logo.png', 'image/png')
+      product = Product.create!(name: 'TV 32 Samsung', width: '30', height: '20', weight: '15', depth: '10',
+                                prod_category: category, logo:, status: :selected)
+      product2 = Product.create!(name: 'SOUNDBAR SONY', width: '30', height: '20', weight: '15', depth: '10',
+                                 prod_category: category2, logo:, status: :selected)
+
+      LotItem.create!(lot:, product:)
+      LotItem.create!(lot: lot2, product: product2)
+
+      Bid.create!(lot:, user: user2, value: 400)
+      Bid.create!(lot:, user: user2, value: 800)
+
+      login_as user, scope: :user
+
+      visit root_path
+
+      click_on lot.code
+
+      click_on 'Dar lance'
+
+      fill_in 'Valor',	with: '500'
+      click_on 'Criar Lance'
+
+      expect(page).to have_content 'Não foi possível fazer o lance'
+      expect(page).to have_content 'A diferença de valor para esse lance é de R$400'
+    end
   end
 end
